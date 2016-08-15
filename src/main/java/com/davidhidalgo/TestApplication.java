cd7ec2c3f0375484fce7323702f8d8b791847bff
David Hidalgo <davidmhf@gmail.com>
2016-08-15 00:00:23 -0500

    Initial commit

---
+++ src/main/java/com/davidhidalgo/TestApplication.java
@@ -0,0 +1,59 @@
+package com.davidhidalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+	
+	
+	
+	@RequestMapping(path = "/test", method = RequestMethod.GET)
+	public ResponseEntity<Response> response(){
+	
+		return new ResponseEntity<>(new Response("David", "25"),HttpStatus.ACCEPTED);
+	}
+}
+
+
+class Response{
+
+
+	String name;
+	
+	String age;
+	
+	public Response(String name, String age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	
+}
